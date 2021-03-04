fdcbdc0655a4a64422c952f71ec2f2c0eeab51e5
Lane Garner <b.lane.garner@gmail.com>
2021-03-04 03:00:56 -0600

    scss fix

---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+import Head from "../components/head"
+
+const AboutPage = () => {
+    return (
+        <Layout>
+            <Head title="Portfolio"/>
+            <h1>Portfolio</h1>
+            <p>My portfolio</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
