365f37dd6a1725d2472a545807645048152de19a
Lane Garner <b.lane.garner@gmail.com>
2021-05-28 16:14:26 -0500

    added styled components to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-source-filesystem",
       options: {
