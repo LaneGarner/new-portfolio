683054dd66669502e5f2a291f70651e04f769e60
Lane Garner <b.lane.garner@gmail.com>
2022-01-03 13:07:50 -0600

    fix blog post margin mobile

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -4,6 +4,7 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import Head from "../components/head"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 import * as blogPostStyles from "./blogpost.module.scss"
 
